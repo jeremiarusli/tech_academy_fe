0e89dd61e6341eb06f437a743d0e727c8b4750fe
Jeremia Varian Rusli <jeremia.rusli@ralali.com>
2019-09-12 18:58:22 +0700

    editing layout

--- jeremy/Latihan01/layouts/default.vue
+++ jeremy/Latihan01/layouts/default.vue
@@ -7,7 +7,7 @@
 <style>
 html {
   font-family: Arial, Helvetica, sans-serif;
-  font-size: 16px;
+  font-size: 15px;
   word-spacing: 1px;
   -ms-text-size-adjust: 100%;
   -webkit-text-size-adjust: 100%;
