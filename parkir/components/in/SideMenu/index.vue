84d26395fc2301c0b58e4895c9b97e95b1ce9d96
Condro Wiyono <condro.wiyono@ralali.com>
2019-09-12 17:00:00 +0700

    scafold layout

---
+++ parkir/components/in/SideMenu/index.vue
@@ -0,0 +1,5 @@
+<template src="./SideMenu.html" />
+<script src="./SideMenu.js">
+</script>
+<style src="./SideMenu.css">
+</style>
---
+++ parkir/components/in/TopMenu/index.vue
@@ -0,0 +1,5 @@
+<template src="./TopMenu.html" />
+<script src="./TopMenu.js">
+</script>
+<style src="./TopMenu.css">
+</style>
